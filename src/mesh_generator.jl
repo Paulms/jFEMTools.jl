baeb04620d3662f43af8da6b78a868ca10455a94
Paul Mendez <pmendez@ing-mat.udec.cl>
2019-07-15 17:26:30 +0100

    Test other polygonal meshes
    Basic description and plots

--- src/mesh_generator.jl
+++ src/mesh_generator.jl
@@ -72,3 +72,49 @@ function rectangle_mesh(::Type{TriangleCell}, nel::NTuple{2,Int}, LL::Tensors.Ve
 
     return PolytopeMesh(cells, nodes; edgesets = edgesets)
 end
+
+#########################
+# Rectangle Cells 2D   #
+#########################
+@inline _build_cell(::Type{RectangleCell}, el_nodes, el_faces) = RectangleCell(el_nodes,(el_faces[1],el_faces[2],el_faces[3],el_faces[4]))
+"""
+rectangle_mesh(::Type{RectangleCell}, nel::NTuple{2,Int}, LL::Vec{2,T}, UR::Vec{2,T}) where {T}
+Generate a rectangular mesh with triangular cells, where `LL` is the low left vertex
+and `UR` is the upper right one. `nel` is a tuple with the number of partions to be
+used in each dimension.
+"""
+function rectangle_mesh(::Type{RectangleCell}, nel::NTuple{2,Int}, LL::Tensors.Vec{2,T}, UR::Tensors.Vec{2,T}) where {T}
+    LR = Tensors.Vec{2}((UR[1],LL[2]))
+    UL = Tensors.Vec{2}((LL[1],UR[2]))
+    nel_x = nel[1]; nel_y = nel[2]; nel_tot = nel_x*nel_y
+    n_nodes_x = nel_x + 1; n_nodes_y = nel_y + 1
+    n_nodes = n_nodes_x * n_nodes_y
+
+    # Generate nodes
+    nodes = Node{2,T}[]
+    _generate_2d_nodes!(nodes, n_nodes_x, n_nodes_y, LL, LR, UR, UL)
+
+    # Generate cells
+    node_array = reshape(collect(1:n_nodes), (n_nodes_x, n_nodes_y))
+    cells = RectangleCell[]
+    for j in 1:nel_y, i in 1:nel_x
+        push!(cells, RectangleCell((node_array[i,j], node_array[i+1,j], node_array[i+1,j+1], node_array[i,j+1])))
+    end
+
+    # Cell faces
+    cell_array = reshape(collect(1:nel_tot),(nel_x, nel_y))
+    boundary = EdgeIndex[[EdgeIndex(cl, 1) for cl in cell_array[:,1]];
+                      [EdgeIndex(cl, 2) for cl in cell_array[end,:]];
+                      [EdgeIndex(cl, 3) for cl in cell_array[:,end]];
+                      [EdgeIndex(cl, 4) for cl in cell_array[1,:]]]
+    # Cell face sets
+    offset = 0
+    edgesets = Dict{String, Set{EdgeIndex}}()
+    edgesets["bottom"] = Set{EdgeIndex}(boundary[(1:length(cell_array[:,1]))   .+ offset]); offset += length(cell_array[:,1])
+    edgesets["right"]  = Set{EdgeIndex}(boundary[(1:length(cell_array[end,:])) .+ offset]); offset += length(cell_array[end,:])
+    edgesets["top"]    = Set{EdgeIndex}(boundary[(1:length(cell_array[:,end])) .+ offset]); offset += length(cell_array[:,end])
+    edgesets["left"]   = Set{EdgeIndex}(boundary[(1:length(cell_array[1,:]))   .+ offset]); offset += length(cell_array[1,:])
+    edgesets["boundary"] = union(edgesets["bottom"],edgesets["right"],edgesets["top"],edgesets["left"])
+
+    return PolytopeMesh(cells, nodes; edgesets = edgesets)
+end
--- test/test_2d_generators.jl
+++ test/test_2d_generators.jl
@@ -16,3 +16,26 @@ for row in 1:getncells(mesh)
 	poly!(scene, points, strokewidth = 1, color = :white, strokecolor = :black, show_axis = true, scale_plot = false)
 end
 display(scene)
+
+# Quad mesh
+mesh = rectangle_mesh(RectangleCell, (2,2), Tensors.Vec{2}((0.0,0.0)), Tensors.Vec{2}((1.0,1.0)))
+
+# Mixed mesh
+cells = [
+	TriangleCell((1,2,4)),
+	RectangleCell((2,3,6,5)),
+	TriangleCell((5,4,2)),
+    RectangleCell((4, 5, 8, 7)),
+	RectangleCell((5,6,9,8))
+    ];
+mixmesh = PolytopeMesh(cells, mesh.nodes)
+
+coordinates = get_vertices_matrix(mixmesh);
+connectivity = get_conectivity_list(mixmesh);
+scene = Scene()
+for row in 1:getncells(mixmesh)
+	#read coordinates
+	points = node(:poly, Point2f0[coordinates[node,:] for node in connectivity[row]])
+	poly!(scene, points, strokewidth = 1, color = :white, strokecolor = :black, show_axis = true, scale_plot = false)
+end
+display(scene)
